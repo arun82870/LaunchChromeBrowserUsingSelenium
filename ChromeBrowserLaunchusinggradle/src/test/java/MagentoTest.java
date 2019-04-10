232e7f96000ff875a964c0c1d6b51cf233d4c9c7
Arun Gupta <Arun Gupta@LAPTOP-7JHQ6NNG>
2019-04-10 20:53:47 +0530

    fourth commit

--- ChromeBrowserLaunchusinggradle/src/test/java/MagentoTest.java
+++ ChromeBrowserLaunchusinggradle/src/test/java/MagentoTest.java
@@ -1,15 +1,14 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MagentoTest {
 	
 	public static void main(String[] args )
 	{
 		
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
 }
